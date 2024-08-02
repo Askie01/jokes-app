a5f3ef7a5e04827b21b978e4a5b25ec09bdbeb8e
Askie01 <adam.paw01@gmail.com>
2024-08-02 18:18:11 +0200

    Initialize repository.
    Initialized repository with default Spring boot files.

---
+++ src/main/java/com/askie01/jokesapp/JokesAppApplication.java
@@ -0,0 +1,12 @@
+package com.askie01.jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesAppApplication.class, args);
+    }
+}
